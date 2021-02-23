e2b4567e819192534d59075f38c5d10e05101a78
Andrey Gridyaev <agridyaev@gmail.com>
2021-02-23 17:04:12 +0300

    add examples for titles, descriptions and severity markers

--- tests/test_07_markers_bdd.py
+++ tests/test_07_markers_bdd.py
@@ -4,7 +4,6 @@ import pytest
 
 @allure.feature("Authorization")
 @allure.story("Valid credentials")
-@allure.title("Set valid credentials")
 @pytest.mark.parametrize("x", [True, 1])
 def test_auth_valid(x):
     assert x
@@ -12,7 +11,6 @@ def test_auth_valid(x):
 
 @allure.feature("Authorization")
 @allure.story("Invalid credentials")
-@allure.title("Set invalid credentials")
 @pytest.mark.parametrize("x", [True, 0])
 def test_auth_non_valid(x):
     assert x
